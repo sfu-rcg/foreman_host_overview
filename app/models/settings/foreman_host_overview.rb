daa515b28f8b2bc0ed488960db8a033cf3029d07
Drasang <tobias.l@gmx.de>
2017-09-18 20:31:41 +0200

    Add setting to switch between ip or fqdn for connection links

--- app/models/settings/foreman_host_overview.rb
+++ app/models/settings/foreman_host_overview.rb
@@ -2,13 +2,22 @@ class Setting::ForemanHostOverview < ::Setting
   def self.load_defaults
     return unless ActiveRecord::Base.connection.table_exists?('settings')
     return unless super
-    
+
     Setting.transaction do
       [
-        self.set('display_non_native_connections', N_('Whether or not to display non-native OS connections'), true)
+        self.set(
+          'display_non_native_connections',
+          N_('Whether or not to display non-native OS connections'),
+          true
+        ),
+        self.set(
+          'use_ip_for_connections',
+          N_('Use ip address for connection links instead of fqdn'),
+          false
+        )
       ].compact.each { |s| self.create! s.update(:category => 'Setting::ForemanHostOverview') }
     end
-    
+
     true
   end
 end
